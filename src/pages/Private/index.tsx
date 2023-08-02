922508c920126df9032a6757582e32bf2417912c
Rafael <rafael-srv@hotmail.com>
2023-08-01 22:45:55 -0300

    Usando axios

--- src/pages/Private/index.tsx
+++ src/pages/Private/index.tsx
@@ -6,7 +6,7 @@ export const Private = () => {
     return(
         <div>
             <h2>Página privada</h2>
-            Olá {auth.user?.name}, Tudo bem!?!!@!!!!!@#@4!
+            Olá {auth.user?.name}, Tudo bem!?!!@!!!!!@#@4!@
         </div>
     )
 }
