eba0dde6b971ee61a6cda4212aa2344467ecdc80
Rafael <rafael-srv@hotmail.com>
2023-08-17 23:47:45 -0300

    Usando axios

--- src/contexts/Auth/AuthContext.tsx
+++ src/contexts/Auth/AuthContext.tsx
@@ -7,4 +7,4 @@ export type AuthContextType = {
     signout: () => void;
 }
 
-export const AuthContext = createContext<AuthContextType>(null!);//criAando contexto
+export const AuthContext = createContext<AuthContextType>(null!);//c#riAando contexto
